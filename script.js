cccadb7d76a051bbecd820aa2689ef63ad718235
unknown <racielramos955@gmail.com>
2026-02-12 22:37:28 -0400

    Actualizacion 4 - Amira Ramos

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function mostrarMensaje() {
     alert("Hola Nayra 😊 Este proyecto está funcionando correctamente!");
 }
+<script>
+        function actualizarFecha() {
+            const hoy = new Date();
+            document.getElementById("fecha").innerText = hoy.toLocaleDateString();
+        }
+
+        actualizarFecha();
+</script>
